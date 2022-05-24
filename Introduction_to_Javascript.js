ace430f736653fbf4c44c82e89975c7eb371f1c3
sakinasyed <sakinasyed05@gmail.com>
2022-05-24 04:56:18 -0700

    assignment 1

---
+++ Introduction_to_Javascript.js
@@ -0,0 +1,24 @@
+// 1. Print Hello World! in the console below.
+console.log("Hello World!");
+
+
+// 2. Use what you learned in the above problem to print the input parameter to the console. (Input is already declared)
+var string = "5";
+    console.log(string);
+
+
+// 3. In the editor below, declare a variable named newVariable and assign it the following string, "AlmaBetter"
+var newVariable = "AlmaBetter";
+console.log(newVariable);
+
+
+// 4.  Type conversion
+// Implicit type conversion: Declare string and add integer. Notice how javascript implicitly converts integer to string.
+// Explicit type conversion: Declare string and add integer by explicitly converting string to integer.
+let number = "777";
+    let integer = 8;
+    console.log(number+integer);
+   
+    let a='785';
+    a=+'785';
+    console.log(a); 
